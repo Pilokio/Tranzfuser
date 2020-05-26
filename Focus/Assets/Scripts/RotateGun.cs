8012b75d91efd1c8682a038cd3dd34d763e38b33
Pilokio <calum.murray98@outlook.com>
2020-05-26 18:15:50 +0100

    Added Unity Project

---
+++ Focus/Assets/Scripts/RotateGun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGun : MonoBehaviour
+{
+    public GrapplingGun grappling;
+
+    private Quaternion desiredRotation;
+    private float rotationSpeed = 5f;
+
+    private void Update()
+    {
+        if (!grappling.IsGrappling())
+        {
+            desiredRotation = transform.parent.rotation;
+        }
+        else
+        {
+            desiredRotation = Quaternion.LookRotation(grappling.GetGrapplePoint() - transform.position);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * rotationSpeed);
+    }
+}
