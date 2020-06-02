717b6c93344691cb10fbb3daa269af40aae12503
BrodieT <35993664+BrodieT@users.noreply.github.com>
2020-06-02 22:34:12 +0100

    Checking Gitignore

---
+++ Focus/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    public Transform player;
+
+    void Update() 
+    {
+        transform.position = player.transform.position;
+    }
+}
