cdfbdb1bbaebe63ddd8d8e1f1b33a65ee9122421
Pilokio <calum.murray98@outlook.com>
2020-06-09 15:35:14 +0100

    Ladders impllemented but a bug is present when tring to descend them

---
+++ Focus/Assets/Scripts/Ladder.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    // Speed the player will ascend/descend the ladder
+    public float speed = 6;
+
+    public GameObject player;
+
+    private void OnTriggerStay(Collider other)
+    {
+        // Ascend ladder
+        if (other.tag == "Player" && Input.GetKey(KeyCode.W))
+        {
+            //Debug.Log("Touching ladder");
+            Physics.gravity = new Vector3(0, 0, 0); //Invert
+            player.GetComponent<Rigidbody>().velocity = new Vector3(0, speed, 0);
+        }
+
+        // Descend ladder
+        // Currently not working
+        if (other.tag == "Player" && Input.GetKey(KeyCode.S))
+        {
+            Physics.gravity = new Vector3(0, 0, 0); //Invert
+            player.GetComponent<Rigidbody>().velocity = new Vector3(0, -speed, 0);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Physics.gravity = new Vector3(0, -30f, 0); //Invert
+    }
+}
