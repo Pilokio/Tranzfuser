20591b5587262c83cb55a5ef3f41ed376746cea8
Pilokio <calum.murray98@outlook.com>
2020-06-26 15:34:24 +0100

    Slow motion started again

--- Focus/Assets/Scripts/Character/Player/PlayerMovement.cs
+++ Focus/Assets/Scripts/Character/Player/PlayerMovement.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : BaseBehaviour
 {
 
     // Movement
---
+++ Focus/Assets/Scripts/Time/TimeControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Chronos;
+
+public class TimeControl : MonoBehaviour
+{
+    void Update()
+    {
+        // Get the Enemies global clock
+        Clock clock = Timekeeper.instance.Clock("Player");
+
+        // Change its time scale on key press
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            clock.localTimeScale = -1; // Rewind
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            clock.localTimeScale = 0; // Pause
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            clock.localTimeScale = 0.1f; // Slow
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            clock.localTimeScale = 1; // Normal
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            clock.localTimeScale = 2; // Accelerate
+        }
+    }
+}
