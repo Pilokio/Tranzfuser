978184f55d53f6686d6be1b121128ce3fa3aee8d
Calum Murray <calum.murray98@outlook.com>
2020-08-12 19:07:31 +0100

    Minor changes

---
+++ Focus/Assets/Scripts/Platforms/Platform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
+    }
+}
