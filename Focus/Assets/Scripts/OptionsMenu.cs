78c150c1e8239f446a75d14d03d7b9b1ab8e7b16
Calum Murray <calum.murray98@outlook.com>
2020-08-19 08:38:17 +0100

    Added sound to main menu

--- Focus/Assets/Scripts/OptionsMenu.cs
+++ Focus/Assets/Scripts/OptionsMenu.cs
@@ -46,7 +46,8 @@ public class OptionsMenu : MonoBehaviour
 
     public void SetVolume (float volume)
     {
-        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+        //Mathf.Log10(volume) * 20
+        audioMixer.SetFloat("volume", volume);
     }
 
     public void SetQuality (int qualityIndex)
