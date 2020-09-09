6d8b0bfc5907013d64b5a4958212d949bae71d50
BrodieT <35993664+BrodieT@users.noreply.github.com>
2020-09-09 15:17:14 +0100

    Added laser grids with respawn colliders

---
+++ Focus/Assets/Scripts/EnemyKillCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyKillCheck : MonoBehaviour
+{
+    [SerializeField] List<EnemyStats> EnemiesInRoom = new List<EnemyStats>();
+    [SerializeField] GameObject LaserGrid;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsAllDead())
+        {
+            Debug.Log("All enemies dead");
+            LaserGrid.SetActive(false);
+        }
+    }
+
+    bool IsAllDead()
+    {
+        foreach (EnemyStats enemy in EnemiesInRoom)
+        {
+            if (!enemy.IsDead)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
